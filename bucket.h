93649945cac7bcd358aff9139e4f756758db6803
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-04-17 18:07:43 +1000

    Made nested classes public

--- bucket.h
+++ bucket.h
@@ -3,39 +3,40 @@
 
 template <class T, class V>
 class BucketSorter {
-    template <class T2, class V2>
-    class Bucket {
-        template <class V3>
-        class Node {
+    public:
+        template <class T2, class V2>
+        class Bucket {
             public:
-                V3 value;
-                Node* next;
+                template <class V3>
+                class Node {
+                    public:
+                        V3 value;
+                        Node* next;
 
-                Node(V3);
+                        Node(V3);
+                };
+
+                T2 identifier;
+                Node<V2>* head;
+                Node<V2>* tail;
+                Bucket<T2, V2>* next;
+
+                void insert(V2);
+                void remove(V2);
+
+                Bucket();
+                Bucket(T2);
+                ~Bucket();
+
+                // Debugging
+                void print();
+
+            private:
+                void Construct(T2);
         };
 
-        public:
-            T2 identifier;
-            Node<V2>* head;
-            Node<V2>* tail;
-            Bucket<T2, V2>* next;
-
-            void insert(V2);
-            void remove(V2);
-
-            Bucket();
-            Bucket(T2);
-            ~Bucket();
-
-            // Debugging
-            void print();
-
-        private:
-            void Construct(T2);
-    };
-    protected:
         Bucket<T, V>* head;
-    public:
+
         BucketSorter();
         ~BucketSorter();
 
