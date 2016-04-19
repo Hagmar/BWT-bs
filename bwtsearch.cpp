a44cac38171895f90a686627a6009e11a45c3f48
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-04-19 17:58:11 +1000

    Occurrence function

--- bwtsearch.cpp
+++ bwtsearch.cpp
@@ -3,6 +3,50 @@
 #include "bwtsearch.h"
 #include "occindex.h"
 
+unsigned int occ(unsigned char c, unsigned int q, OccIndex* occIndex,
+        std::istream& in){
+    OccIndex::OccBlock* block = getIndexBlock(c, q, occIndex);
+
+    unsigned int occurrences = 0;
+    unsigned int i = 0;
+
+    in.clear();
+    if (block){
+        in.seekg(block->position, in.beg);
+        i = block->position;
+        occurrences = block->occInBlock(c);
+    } else {
+        in.seekg(0, in.beg);
+    }
+
+    char readChar;
+    for (; i < q; i++){
+        if (!in.get(readChar)){
+            break;
+        }
+        if (c == readChar){
+            occurrences++;
+        }
+    }
+    return occurrences;
+}
+
+OccIndex::OccBlock* getIndexBlock(unsigned char c, unsigned int q, OccIndex* occIndex){
+    OccIndex::OccBlock* block = occIndex->head;
+    while (block->position < q){
+        if (block->next){
+            block = block->next;
+        } else {
+            break;
+        }
+    }
+    if (block->position < q){
+        return block;
+    } else {
+        return NULL;
+    }
+}
+
 int main(int argc, char** argv){
     if (argc != 5){
         std::cerr << "Error, wrong number of arguments" << std::endl;
@@ -14,6 +58,9 @@ int main(int argc, char** argv){
 
         OccIndex* occIndex = new OccIndex();
         occIndex->createOccIndex(argv[2]);
+
+        std::ifstream in(argv[2]);
+        std::cout << occ('s', 9, occIndex, in) << std::endl;
     }
     return 0;
 }
--- bwtsearch.h
+++ bwtsearch.h
@@ -1,6 +1,11 @@
 #ifndef BWTSEARCH_H
 #define BWTSEARCH_H
 
+#include "occindex.h"
+
+unsigned int occ(unsigned char, unsigned int, OccIndex*, std::istream&);
+OccIndex::OccBlock* getIndexBlock(unsigned char, unsigned int, OccIndex*);
+
 int main(int, char**);
 
 #endif
