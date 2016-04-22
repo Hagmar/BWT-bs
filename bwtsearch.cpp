198dc057269a3deed4cd1713a6af213ddd046465
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-04-22 15:01:54 +1000

    Changed search to use Index class instead of the separate ones

--- bwtsearch.cpp
+++ bwtsearch.cpp
@@ -3,40 +3,13 @@
 #include <cstring>
 #include <set>
 #include "bwtsearch.h"
+#include "index.h"
 #include "occindex.h"
 #include "ctable.h"
 
-unsigned int occ(unsigned char c, unsigned int q, OccIndex* occIndex,
-        std::istream& in){
-    OccIndex::OccBlock* block = getIndexBlock(c, q, occIndex);
-
-    unsigned int occurrences = 0;
-    unsigned int i = 0;
-
-    in.clear();
-    if (block){
-        in.seekg(block->position, in.beg);
-        i = block->position;
-        occurrences = block->occInBlock(c);
-    } else {
-        in.seekg(0, in.beg);
-    }
-
-    char readChar;
-    for (; i <= q; i++){
-        if (!in.get(readChar)){
-            break;
-        }
-        if (c == readChar){
-            occurrences++;
-        }
-    }
-    return occurrences;
-}
 
 // TODO Segfaults on empty pattern
-searchResult backwardSearch(const char* pattern, const char* filename,
-        OccIndex* index, CTable* cTable){
+searchResult backwardSearch(const char* pattern, const char* filename, Index* index){
     searchResult result;
     result.first = 0;
     result.last = 0;
@@ -45,20 +18,20 @@ searchResult backwardSearch(const char* pattern, const char* filename,
 
     unsigned int i = std::strlen(pattern) - 1;
     unsigned char c = pattern[i];
-    result.first = cTable->getC(c);
-    result.last = cTable->getC(c+1) - 1;
+    result.first = index->getC(c);
+    result.last = index->getC(c+1) - 1;
     unsigned char occurrences;
     while ((result.first <= result.last) && i){
         c = pattern[--i];
         if (result.first){
-            occurrences = occ(c, result.first-1, index, in);
+            occurrences = index->occ(c, result.first-1, in);
         } else {
             in.clear();
             in.seekg(0, in.beg);
             occurrences = in.get() == c;
         }
-        result.first = cTable->getC(c) + occurrences;
-        result.last = cTable->getC(c) + occ(c, result.last, index, in) - 1;
+        result.first = index->getC(c) + occurrences;
+        result.last = index->getC(c) + index->occ(c, result.last, in) - 1;
     }
 
     in.close();
@@ -66,7 +39,7 @@ searchResult backwardSearch(const char* pattern, const char* filename,
 }
 
 void interpretResults(searchResult result, const char* mode,
-        const char* filename, OccIndex* occIndex, CTable* cTable){
+        const char* filename, Index* index){
     if (!std::strcmp(mode, "-n")){
         if (result.last < result.first){
             std::cout << 0 << std::endl;
@@ -76,7 +49,7 @@ void interpretResults(searchResult result, const char* mode,
     } else {
         bool listRecords = std::strcmp(mode, "-r");
         std::set<unsigned int>* records;
-        records = findRecords(result, filename, occIndex, cTable);
+        records = findRecords(result, filename, index);
         if (listRecords){
             std::set<unsigned int>::iterator it = records->begin();
             while (it != records->end()){
@@ -88,8 +61,8 @@ void interpretResults(searchResult result, const char* mode,
     }
 }
 
-std::set<unsigned int>* findRecords(searchResult result, const char* filename,
-        OccIndex* occIndex, CTable* cTable){
+std::set<unsigned int>* findRecords(searchResult result,
+        const char* filename, Index* index){
     std::ifstream in(filename);
     unsigned int record;
     unsigned int scale;
@@ -103,7 +76,7 @@ std::set<unsigned int>* findRecords(searchResult result, const char* filename,
         scale = 1;
         in.seekg(hit);
         in.get(c);
-        next = occ(c, hit, occIndex, in) + cTable->getC(c) - 1;
+        next = index->occ(c, hit, in) + index->getC(c) - 1;
         while (c != '['){
             if (c == ']'){
                 reachedOffset = true;
@@ -113,7 +86,7 @@ std::set<unsigned int>* findRecords(searchResult result, const char* filename,
             }
             in.seekg(next);
             in.get(c);
-            next = occ(c, next, occIndex, in) + cTable->getC(c) - 1;
+            next = index->occ(c, next, in) + index->getC(c) - 1;
         }
         records->insert(record);
     }
@@ -126,20 +99,16 @@ int main(int argc, char** argv){
     } else {
         const char* mode = argv[1];
         const char* filename = argv[2];
-        const char* index = argv[3];
+        const char* indexFile = argv[3];
         const char* pattern = argv[4];
 
-        OccIndex* occIndex = new OccIndex();
-        occIndex->createOccIndex(filename);
+        Index* index = new Index(filename);
 
-        CTable* cTable = new CTable(occIndex);
+        searchResult result = backwardSearch(pattern, filename, index);
+        std::cout << "Search complete" << std::endl;
+        std::cout<<"First: "<<result.first<<", Last: "<<result.last<<std::endl;
 
-        searchResult result = backwardSearch(pattern, filename, occIndex, cTable);
-
-        interpretResults(result, mode, filename, occIndex, cTable);
-
-        delete occIndex;
-        delete cTable;
+        interpretResults(result, mode, filename, index);
     }
     return 0;
 }
--- bwtsearch.h
+++ bwtsearch.h
@@ -2,22 +2,18 @@
 #define BWTSEARCH_H
 
 #include <set>
-#include "occindex.h"
-#include "ctable.h"
+#include "index.h"
 
 struct searchResult {
     unsigned int first;
     unsigned int last;
 };
 
-unsigned int occ(unsigned char, unsigned int, OccIndex*, std::istream&);
 OccIndex::OccBlock* getIndexBlock(unsigned char, unsigned int, OccIndex*);
 
-searchResult backwardSearch(const char*, const char*, OccIndex*, CTable*);
-void interpretResults(searchResult, const char*, const char*, OccIndex*,
-        CTable*);
-std::set<unsigned int>* findRecords(searchResult, const char*, OccIndex*,
-        CTable*);
+searchResult backwardSearch(const char*, const char*, Index*);
+void interpretResults(searchResult, const char*, const char*, Index*);
+std::set<unsigned int>* findRecords(searchResult, const char*, Index*);
 
 int main(int, char**);
 
