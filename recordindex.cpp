c8da92e3a63c1b24ee4ead590834da24c5c85ec6
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-04-22 13:56:07 +1000

    Debugging print function

--- recordindex.cpp
+++ recordindex.cpp
@@ -7,4 +7,11 @@ RecordIndex::RecordIndex(){
 }
 
 void RecordIndex::print(){
+    std::map<unsigned int, unsigned int>::iterator it = index->begin();
+    unsigned int k, v;
+    for (; it != index->end(); it++){
+        k = it->first;
+        v = it->second;
+        std::cout << "K: " << k << ", V: " << v << std::endl;
+    }
 }
