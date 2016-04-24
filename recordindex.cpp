dff2d2c22e4e35f84c1b4452a5bfc5e7491306da
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-04-24 15:33:23 +1000

    Record generation

--- recordindex.cpp
+++ recordindex.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <map>
+#include <list>
 #include "recordindex.h"
 #include "occindex.h"
 #include "ctable.h"
 
 RecordIndex::RecordIndex(){
-    index = new std::map<unsigned int, unsigned int>;
+    index = std::map<unsigned int, unsigned int>();
 }
 
-// TODO
 void RecordIndex::createIndex(OccIndex* occIndex, CTable* cTable,
         std::istream& in){
     in.clear();
@@ -19,15 +19,71 @@ void RecordIndex::createIndex(OccIndex* occIndex, CTable* cTable,
         return;
     }
 
+
+    unsigned int start = 0;
     unsigned int position = 0;
-    unsigned int record;
+    unsigned int record = 0;
+    unsigned int scale = 1;
     unsigned int next;
+    unsigned int characters = 0;
+    std::list<unsigned int>::iterator it;
+
+    next = occIndex->occ(c, position, in) + cTable->getC(c, false) - 1;
+    while (c == '[' || c == ']' || (c >= '0' && c <= '9')){
+        in.seekg(next, in.beg);
+        in.get(c);
+        start = next;
+        next = occIndex->occ(c, next, in) + cTable->getC(c, false) - 1;
+    }
+    std::list<unsigned int> positions (1, start);
+
+    while (next != start){
+        in.seekg(next, in.beg);
+        in.get(c);
+        next = occIndex->occ(c, next, in) + cTable->getC(c, false) - 1;
+    }
+    return;
+
+    while (true){
+        while (c != ']'){
+            if (next == start){
+                for (it = positions.begin(); it != positions.end(); it++){
+                    index[*it] = index[start];
+                }
+                return;
+            }
+            in.seekg(next, in.beg);
+            in.get(c);
+            if (++characters > RECORDINTERVAL){
+                positions.push_front(next);
+                characters = 0;
+            }
+            next = occIndex->occ(c, next, in) + cTable->getC(c, false) - 1;
+        }
+        in.seekg(next, in.beg);
+        in.get(c);
+        next = occIndex->occ(c, next, in) + cTable->getC(c, false) - 1;
+        while (c != '['){
+            record += (c-'0') * scale;
+            scale *= 10;
+            in.seekg(next, in.beg);
+            in.get(c);
+            next = occIndex->occ(c, next, in) + cTable->getC(c, false) - 1;
+        }
+        for (it = positions.begin(); it != positions.end(); it++){
+            index[*it] = record;
+        }
+        positions.clear();
+        record = 0;
+        scale = 1;
+        characters = -1;
+    }
 }
 
 void RecordIndex::print(){
-    std::map<unsigned int, unsigned int>::iterator it = index->begin();
+    std::map<unsigned int, unsigned int>::iterator it = index.begin();
     unsigned int k, v;
-    for (; it != index->end(); it++){
+    for (; it != index.end(); it++){
         k = it->first;
         v = it->second;
         std::cout << "K: " << k << ", V: " << v << std::endl;
--- recordindex.h
+++ recordindex.h
@@ -8,14 +8,14 @@
 // For each entry in the index we need 4 bytes for they key and 4 for the
 // value. This interval needs to be large enough to make sure the index file
 // is small enough.
-#define RECORDINTERVAL 15
+#define RECORDINTERVAL 10
 
 class RecordIndex {
-    std::map<unsigned int, unsigned int>* index;
-
-    void createIndex(OccIndex*, CTable*, std::istream&);
+    std::map<unsigned int, unsigned int> index;
 
     public:
+        void createIndex(OccIndex*, CTable*, std::istream&);
+
         RecordIndex();
         RecordIndex(OccIndex*, CTable*, std::istream&);
         ~RecordIndex();
